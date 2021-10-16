6472328d5913646423d0c82ea9db46fb4849ad48
simenwai <simenwai@stud.ntnu.no>
2021-10-16 20:53:15 +0200

    changed text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Dette er simen sin app
         </p>
         <a
           className="App-link"
